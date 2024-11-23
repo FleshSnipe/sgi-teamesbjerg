b4c798a63f1a45d2418e2f308788b5d04e3d4a5a
matyasjanosek <matyas.janosek@gmail.com>
2024-11-23 15:20:18 +0100

    update klubben.php

--- klubben.php
+++ klubben.php
@@ -4,24 +4,19 @@ get_header();
 ?>
 
 <div class="container mx-auto max-w-7xl lg:h-[90vh] lg:py-0 py-20 flex justify-center">
-    <div class="flex lg:flex-row md:flex-col flex-col justify-center items-center">
-        <div class="lg:max-w-xl md:max-w-lg max-w-sm lg:px-0 md:px-0 px-5">
-            <h1 class="lg:text-4xl md:text-3xl text-2xl font-bold text-[#151517] lg:mb-8 mb-4">
-                <?php the_title(); ?>
-            </h1>
-            <p class="lg:text-base md:text-sm text-xs">
-                <?php
-                // Dynamický obsah stránky
-                while (have_posts()) : the_post();
-                    the_content();
-                endwhile;
-                ?>
-            </p>
-        </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/images/logoCombined.png" alt="logoCombined" class="h-auto lg:w-[650px] md:w-[550px] w-[450px]">
+        <div class="flex lg:flex-row md:flex-col flex-col justify-center items-center">
+            <div class="lg:max-w-xl md:max-w-lg max-w-sm lg:px-0 md:px-0 px-5">
+                <h1 class="lg:text-4xl md:text-3xl text-2xl font-bold text-[#151517] lg:mb-8 mb-4">
+                    Vores historie
+                </h1>
+                <p class="lg:text-base md:text-sm text-xs">
+                    I 2022 blev der taget et aktivt valg om at sikre kvaliteten af drengehåndbolden i Esbjerg. I årene efter Corona havde drengehåndbold haft en hård skæbne. Initiativet blev taget for at lave denne skæbne om. Det skete med et holdfællesskab hos klubberne Team Esbjerg HK og SGI Håndbold. Det første år var svært og en plads i U17 A-rækken gjorde det ikke så attraktivt at være en del af. Vi var bevidste om, at det var en start og projektet skulle bruge tid til at komme på benene. Vi er nu i gang med andet år og står nu med to hold: U17 i U17 2. Division og U19 1. Division. Holdene består af mange spillere fra selv samme hold, men har også fået tilgang siden hen. Det fortæller kort og godt, hvordan vores holdning er: fokus på et sportsligt setup, der udvikler alle. Det har siden hen givet flere af vores spillere mulighed for at vise sig frem til regional talenttræning, national talentræning og en udtagelse til landsholdssamling. Kursen er sat og vi har ikke planer om at ændre den. Vi vil have drengehåndbolden i Esbjerg!
+                </p>
+            </div>
+            <div>
+                <img src="images/logoCombined.png" alt="logoCombined" class="h-auto lg:w-[650px] md:w-[550px] w-[450px]">
+            </div>
         </div>
     </div>
-</div>
 
 <?php get_footer(); ?>
