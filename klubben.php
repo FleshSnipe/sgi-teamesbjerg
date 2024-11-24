5b8df9a0bfbccf684a8b52d08ea3ef7544f02edf
patrik-sevcik <patsev01@easv365.dk>
2024-11-25 00:25:19 +0100

    as

--- klubben.php
+++ klubben.php
@@ -7,7 +7,7 @@ get_header();
         <div class="flex flex-col items-center justify-center lg:flex-row md:flex-col">
             <div class="max-w-sm px-5 lg:max-w-xl md:max-w-lg lg:px-0 md:px-0">
                 <h1 class="lg:text-4xl md:text-3xl text-2xl font-bold text-[#151517] lg:mb-8 mb-4">
-                    Vores historie
+                    Vores historiee
                 </h1>
                 <p class="text-xs lg:text-base md:text-sm">
                     I 2022 blev der taget et aktivt valg om at sikre kvaliteten af drengehåndbolden i Esbjerg. I årene efter Corona havde drengehåndbold haft en hård skæbne. Initiativet blev taget for at lave denne skæbne om. Det skete med et holdfællesskab hos klubberne Team Esbjerg HK og SGI Håndbold. Det første år var svært og en plads i U17 A-rækken gjorde det ikke så attraktivt at være en del af. Vi var bevidste om, at det var en start og projektet skulle bruge tid til at komme på benene. Vi er nu i gang med andet år og står nu med to hold: U17 i U17 2. Division og U19 1. Division. Holdene består af mange spillere fra selv samme hold, men har også fået tilgang siden hen. Det fortæller kort og godt, hvordan vores holdning er: fokus på et sportsligt setup, der udvikler alle. Det har siden hen givet flere af vores spillere mulighed for at vise sig frem til regional talenttræning, national talentræning og en udtagelse til landsholdssamling. Kursen er sat og vi har ikke planer om at ændre den. Vi vil have drengehåndbolden i Esbjerg!
