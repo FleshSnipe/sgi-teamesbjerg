9c416607475474a5b358c378064469b9fd36a5aa
patrik-sevcik <patsev01@easv365.dk>
2024-11-24 11:57:23 +0100

    partnavbar

--- header.php
+++ header.php
@@ -18,35 +18,35 @@
     <?php wp_body_open(); ?>
     <div class="bg-[#e3252d] lg:sticky static top-0 z-50 opacity-[90%]">
         <div class="container py-4 mx-auto max-w-7xl lg:py-0">
-            <div class="relative flex items-center justify-end py-4 pr-4 lg:justify-between lg:pr-0">
+            <div id="navbar" class="relative flex items-center justify-end py-4 pr-4 lg:justify-between lg:pr-0">
                 <!-- Logo -->
                 <div class="absolute transform -translate-x-1/2 lg:static left-1/2 lg:translate-x-0 lg:pr-0 pr-28">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/teamEsbjerg.png" alt="teamEsbjergHandball" class="w-auto h-20">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/teamEsbjerg.png" alt="teamEsbjergHandball" class="w-auto h-16 lg:h-20 md:h-20">
                 </div>
                 <div class="hidden lg:block">
-                    <ul class="flex space-x-16 text-lg font-medium text-white">
+                    <ul class="flex flex-col space-x-16 text-lg font-medium text-white lg:flex-row">
                         <li>
                             <a href="<?php echo home_url('/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 active:after:w-full focus:after:w-full">
                                 Forside
                             </a>
                         </li>
                         <li>
-                            <a href="<?php echo home_url('/nyheder/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 focus:after:w-full active:after:w-full">
+                            <a href="<?php echo home_url('/nyheder/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 active:after:w-full focus:after:w-full">
                                 Nyheder
                             </a>
                         </li>
                         <li>
-                            <a href="<?php echo home_url('/klubben/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 focus:after:w-full active:after:w-full">
+                            <a href="<?php echo home_url('/klubben/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 active:after:w-full focus:after:w-full">
                                 Klubben
                             </a>
                         </li>
                         <li>
-                            <a href="<?php echo home_url('/sponsorer/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 focus:after:w-full active:after:w-full">
+                            <a href="<?php echo home_url('/sponsorer/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 active:after:w-full focus:after:w-full">
                                 Sponsorer
                             </a>
                         </li>
                         <li>
-                            <a href="<?php echo home_url('/kontakt/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 focus:after:w-full active:after:w-full">
+                            <a href="<?php echo home_url('/kontakt/'); ?>" class="relative after:content-[''] after:absolute after:h-[2px] after:w-0 after:bg-white after:bottom-[-16px] after:left-1/2 after:-translate-x-1/2 after:origin-center hover:after:w-full after:transition-all after:duration-300 active:after:w-full focus:after:w-full">
                                 Kontakt Os
                             </a>
                         </li>
