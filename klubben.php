e171a505cd8cabb3746df26a4e4e7bb244fffe01
patrik-sevcik <patsev01@easv365.dk>
2024-11-25 00:23:43 +0100

    video

--- klubben.php
+++ klubben.php
@@ -4,12 +4,12 @@ get_header();
 ?>
 
 <div class="container mx-auto max-w-7xl lg:h-[90vh] lg:py-0 py-20 flex justify-center">
-        <div class="flex lg:flex-row md:flex-col flex-col justify-center items-center">
-            <div class="lg:max-w-xl md:max-w-lg max-w-sm lg:px-0 md:px-0 px-5">
+        <div class="flex flex-col items-center justify-center lg:flex-row md:flex-col">
+            <div class="max-w-sm px-5 lg:max-w-xl md:max-w-lg lg:px-0 md:px-0">
                 <h1 class="lg:text-4xl md:text-3xl text-2xl font-bold text-[#151517] lg:mb-8 mb-4">
                     Vores historie
                 </h1>
-                <p class="lg:text-base md:text-sm text-xs">
+                <p class="text-xs lg:text-base md:text-sm">
                     I 2022 blev der taget et aktivt valg om at sikre kvaliteten af drengehåndbolden i Esbjerg. I årene efter Corona havde drengehåndbold haft en hård skæbne. Initiativet blev taget for at lave denne skæbne om. Det skete med et holdfællesskab hos klubberne Team Esbjerg HK og SGI Håndbold. Det første år var svært og en plads i U17 A-rækken gjorde det ikke så attraktivt at være en del af. Vi var bevidste om, at det var en start og projektet skulle bruge tid til at komme på benene. Vi er nu i gang med andet år og står nu med to hold: U17 i U17 2. Division og U19 1. Division. Holdene består af mange spillere fra selv samme hold, men har også fået tilgang siden hen. Det fortæller kort og godt, hvordan vores holdning er: fokus på et sportsligt setup, der udvikler alle. Det har siden hen givet flere af vores spillere mulighed for at vise sig frem til regional talenttræning, national talentræning og en udtagelse til landsholdssamling. Kursen er sat og vi har ikke planer om at ændre den. Vi vil have drengehåndbolden i Esbjerg!
                 </p>
             </div>
@@ -17,6 +17,17 @@ get_header();
                 <img src="<?php echo get_template_directory_uri(); ?>/images/logoCombined.png" alt="logoCombined" class="h-auto lg:w-[650px] md:w-[550px] w-[450px]">
             </div>
         </div>
+    </div>
+    <div class="container py-16 mx-auto max-w-7xl">
+        <div class="flex items-center justify-center ">
+            <iframe class="lg:w-[70vh] md:w-[55vh] w-[45vh] h-[20vh] lg:h-[43vh]"
+            src="https://www.youtube.com/embed/u2XTcLqXG0Q"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen></iframe>
+            </div>
     </div>
 
 <?php get_footer(); ?>
