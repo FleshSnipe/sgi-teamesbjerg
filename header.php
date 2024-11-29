6d473311747e3f92182975b3baa2766e1e6475b8
Jakub <jakrep01@easv365.dk>
2024-11-29 16:20:55 +0100

    meta descripiton

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Discover the dynamic handball club in Esbjerg! Stay updated with team news, events, and join our vibrant community. Explore sponsorship opportunities, learn about our history, and follow us on social media for the latest updates.">
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
     <style>
