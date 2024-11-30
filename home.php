bf3f44e1a735d82a3bf34e1c88ab983267a32d5e
patrik-sevcik <patsev01@easv365.dk>
2024-11-30 17:13:30 +0100

    s

--- home.php
+++ home.php
@@ -61,16 +61,22 @@ get_header();
         text-decoration: underline;
     }
 
-    .page-numbers .pagination-prev,
-    .page-numbers .pagination-next {
-        display: inline-flex; /* Zarovnání šipky a textu na jeden řádek */
-        align-items: center; /* Vertikální zarovnání obsahu */
-        justify-content: center; /* Horizontální zarovnání */
-        min-width: 100px; /* Nastavení minimální šířky */
-        text-align: center; /* Zarovnání textu */
-        gap: 0.5rem; /* Mezera mezi šipkou a textem */
+    .page-numbers {
+        display: inline-flex; /* Zarovnání na jeden řádek */
+        gap: 1rem; /* Přidání mezery mezi prvky stránkování */
     }
 
+    .page-numbers .pagination-prev,
+    .page-numbers .pagination-next {
+        min-width: 100px; /* Nastavení minimální šířky pro šipky a text */
+        text-align: center; /* Zarovnání obsahu do středu */
+    }
+
+    .page-numbers span {
+        display: inline-block; /* Zachování správného zobrazení čísla */
+        min-width: 40px; /* Zvětšení šířky jednotlivých čísel */
+        text-align: center; /* Zarovnání čísla */
+    }
 
 </style>
 
