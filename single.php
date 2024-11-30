a1540a2c01beafbb5b36cf4efc4fd8704eeb4bbb
Jakub <jakrep01@easv365.dk>
2024-11-30 17:02:56 +0100

    blog wrapper

--- single.php
+++ single.php
@@ -2,10 +2,10 @@
 get_header();
 ?>
 
-<div class="container mx-auto py-20">
+<div class="container blog-wrapper mx-auto py-20">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article class="prose lg:prose-xl mx-auto">
-            <h1><?php the_title(); ?></h1>
+            <h4><?php the_title(); ?></h4>
             <p class="text-gray-500 text-sm"><?php the_date(); ?> | Autor: <?php the_author(); ?></p>
             <?php if (has_post_thumbnail()) : ?>
                 <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>" class="rounded-lg mt-6">
@@ -37,7 +37,9 @@ get_header();
         color: #687279;
     }
 
- 
+    .blog-wrapper{
+        max-width: 800px;
+    }
 
 </style>
 
